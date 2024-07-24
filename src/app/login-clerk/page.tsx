b51e2b1aea56974ec64dd5a5f5c2942ec6bec787
Bagas Setiaji <stjbagas28@gmail.com>
2024-07-24 14:41:17 +0700

    fix again

--- src/app/login-clerk/page.tsx
+++ src/app/login-clerk/page.tsx
@@ -1,23 +1,32 @@
-import {
-    ClerkProvider,
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton
-} from '@clerk/nextjs'
 
-export default function LoginPage() {
-    return (
-        <ClerkProvider>
-            <div>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <h1>Login Page Content</h1>
-            </div>
-        </ClerkProvider>
-    )
-}
+// import {
+//   ClerkProvider,
+//   SignInButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton
+// } from '@clerk/nextjs'
+// import '../globals.css'
+
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <body>
+//           <SignedOut>
+//             <SignInButton />
+//           </SignedOut>
+//           <SignedIn>
+//             <UserButton />
+//           </SignedIn>
+//           {children}
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   )
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
 import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Dashboard from "./dashboard/page";
+import './globals.css'
 
 export default function Home() {
   return (
-    <Dashboard/>
+      <Dashboard/>
+      // <div>dsare</div>
+    // <div>
+    //   <SignedIn>
+    //     <UserButton afterSignOutUrl="/" />
+    //   </SignedIn>
+    //   <SignedOut>
+    //     <SignInButton />
+    //   </SignedOut>
+    // </div>
   )
 }

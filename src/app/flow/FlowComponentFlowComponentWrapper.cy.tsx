cd1f9a93dabfcaade4f6dcafdee4a42790b37f3f
Bagas Setiaji <stjbagas28@gmail.com>
2024-08-19 21:21:48 +0700

    fix bug

--- src/app/flow/FlowComponentFlowComponentWrapper.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FlowComponentWrapper from './FlowComponent'
-
-describe('<FlowComponentWrapper />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<FlowComponentWrapper />)
-  })
-})
